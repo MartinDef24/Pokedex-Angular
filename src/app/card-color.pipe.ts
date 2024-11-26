6b8ceffb3ebdd866d03e4c4fe0ce99310444b9e1
MartinDefachelles <martin@battlekart.com>
2024-11-26 15:55:56 +0100

    update pokemon card

---
+++ src/app/card-color.pipe.ts
@@ -0,0 +1,54 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'cardColor',
+  standalone: true
+})
+
+export class CardColorPipe implements PipeTransform {
+
+  transform(type:string): unknown {
+    let color: string;
+    switch (type) {
+      case 'Feu':
+        color = 'red';
+        break;
+      case 'Eau':
+        color = 'blue';
+        break;
+      case 'Plante':
+        color = 'green';
+        break;
+      case 'Insecte':
+        color = 'brown';
+        break;
+      case 'Normal':
+        color = 'grey';
+        break;
+      case 'Vol':
+        color = 'blue';
+        break;
+      case 'Poison':
+        color = 'deep-purple';
+        break;
+      case 'Fée':
+        color = 'pink';
+        break;
+      case 'Psy':
+        color = 'deep-purple';
+        break;
+      case 'Electrik':
+        color = 'lime';
+        break;
+      case 'Combat':
+        color = 'deep-orange';
+        break;
+      default:
+        color = 'grey';
+        break;
+    }
+
+    return color;
+  }
+
+}
